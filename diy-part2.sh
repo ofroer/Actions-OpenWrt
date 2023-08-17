0f716e3cb533078db1ae45cc37f88736e83e10b5
ofroer <90967586+ofroer@users.noreply.github.com>
2023-08-17 10:20:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,19 +9,19 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.50.1/192.168.50.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #openclash packages
-#git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 #passwall package 
-#git clone  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
-#git clone -b luci  https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
+git clone  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
+git clone -b luci  https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
 
 #helloworld package
-#git clone -b master  https://github.com/fw876/helloworld.git  package/helloworld
+git clone -b master  https://github.com/fw876/helloworld.git  package/helloworld
 
 #update golang
-#pushd feeds/packages/lang
-#rm -rf golang && svn co https://github.com/openwrt/packages/trunk/lang/golang
-#popd
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/trunk/lang/golang
+popd
